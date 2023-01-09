ca10de4bda9d634382db7ad227a731569c96891d
Olanrewaju Farooq <olanrewajufarooq@yahoo.com>
2023-01-09 06:26:58 +0100

    Selecting the correct surface drifters for Day 2

--- MATLAB/Wind/run.m
+++ MATLAB/Wind/run.m
@@ -5,7 +5,8 @@ wind_data = load('Data/AROME_2022_Oct.mat');
 path = "../../Python/CleanDriftersData/Day2/";
 drifter_data = read_drifter_data(path);
 
-considered_drifters = ["273", "274", "277"];
+% The surface Drifters for Day 2
+considered_drifters = ["8436", "0119", "2052"];
 
 pause_duration = 0.3; % in seconds
 
