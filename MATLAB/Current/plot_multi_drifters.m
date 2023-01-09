f58885d318d378898868dd5123fdfae1b0aefd96
Olanrewaju Farooq <olanrewajufarooq@yahoo.com>
2023-01-09 05:19:43 +0100

    Current at specific points

--- MATLAB/Current/plot_multi_drifters.m
+++ MATLAB/Current/plot_multi_drifters.m
@@ -1,104 +1,138 @@
-function [drifters] = plot_multi_drifters(longs_sim, lats_sim, depth, model_data, deploy_longs, deploy_lats, deploy_time, ret_time, dt )
-    %PLOT_TRAJECTORY Summary of this function goes here
-    %   Detailed explanation goes here
-    
-    % Specify the Depth
-    
-    if strcmp(depth, "Surface")
-        depth_val = 1;
-    elseif strcmp(depth, "60cm Depth")
-        depth_val = 2;
-    elseif strcmp(depth, "1m Depth")
-        depth_val = 3;
+function [drifters] = plot_multi_drifters(longs, lats, deploy_times, recov_times, depth, model_data)
+%PLOT_TRAJECTORY Summary of this function goes here
+%   Detailed explanation goes here
+
+% CONSTANTS
+R = 6370; % Radius of Earth (in km)
+
+start_time = min(datenum(datetime(deploy_times, "InputFormat", "yyyy-MM-dd HH:mm:ss")));
+end_time = max(datenum(datetime(recov_times, "InputFormat", "yyyy-MM-dd HH:mm:ss")));
+
+dt = datenum(datetime("2020-01-01 00:05:00", "InputFormat", "yyyy-MM-dd HH:mm:ss")) - datenum(datetime("2020-01-01 00:0:00", "InputFormat", "yyyy-MM-dd HH:mm:ss"));
+
+% Specify the Depth
+
+if depth == 0.0
+    depth_val = 1;
+elseif depth == 0.6
+    depth_val = 2;
+elseif depth == 1.0
+    depth_val = 3;
+end
+
+% Initialize Output
+
+for i = 1:length(longs)
+    drifters(i).LONG(1) = longs(i);
+    drifters(i).LAT(1) = lats(i);
+    drifters(i).deploy_time = datenum(deploy_times(i));
+    drifters(i).recov_time = datenum(recov_times(i));
+    drifters(i).coasted = false;
+    drifters(i).recovered = false;
+    drifters(i).plot_mode = false;
+    drifters(i).plot_start_index_checked = false;
+end
+
+% Obtain Necessary data
+
+T_model = model_data.time;
+U_model = model_data.U_current; V_model = model_data.V_current;
+LONG_data = model_data.xx; LAT_data = model_data.yy;
+
+% Plot Data
+
+figure(1)
+
+quiverplot = quiver(LONG_data, LAT_data, U_model(:, :, depth_val, 1), V_model(:, :, depth_val, 1), "Color", "b");
+xlabel("Longitudes")
+ylabel("Latitude")
+hold on
+
+T = start_time:dt:end_time;
+time_text = text(5.955, 43.125, "Current time: ");
+
+for t = 1:length(T)-1
+
+    current_time = T(t);
+    time_t = "Current time: " + datestr(datetime(current_time, "ConvertFrom", "datenum"));
+    delete(time_text); time_text = text(5.955, 43.125, time_t);
+
+    for i = 1:length(longs)
+        if (current_time > drifters(i).deploy_time) && (current_time < drifters(i).recov_time)
+            drifters(i).plot_mode = true;
+            if ~drifters(i).plot_start_index_checked
+                drifters(i).plot_start_index = t;
+                drifters(i).plot_start_index_checked = true; 
+            end
+        else
+            drifters(i).plot_mode = false;
+        end
     end
-    
-    % Initialize Output
-    
-    for i = 1:length(longs_sim)
-        drifters(i).LONG(1) = longs_sim(i);
-        drifters(i).LAT(1) = lats_sim(i);
-        drifters(i).coast_check = true;
-    end
-    
-    % Obtain Necessary data
-    
-    T = model_data.time;  z = 0:1: length(T)-1 ; 
-    U = model_data.U_current; V = model_data.V_current; 
-    LONG_data = model_data.xx; LAT_data = model_data.yy;
-    
-    % creating 3D mesh data
-    [y_grid, x_grid, z_grid] = meshgrid(LAT_data(:,1), LONG_data(1,:),z) ;
-    
-    U_mesh = squeeze(U(:,:,depth_val,:)) ;
-    V_mesh = squeeze(V(:,:,depth_val,:)) ;
-    % unit conversions 
-    m2deg_long = 78567 ;
-    m2deg_lat  = 110000 ;
-    h_s = 60*60 ;
-    
-    long_conv = h_s/ m2deg_long ;
-    lat_conv = h_s/ m2deg_lat ;
-    % Plot Data
-    
-    figure(1)
-    
-    quiverplot = quiver(LONG_data, LAT_data, U(:, :, depth_val, 1), V(:, :, depth_val, 1), "Color", "b");
-    xlabel("Longitude")
-    ylabel("Latitude")
-    hold on
-    
-    t_range = deploy_time: dt: ret_time ; 
-    
-    for t = 1: length(t_range) 
-        for i = 1:length(longs_sim)
+
+    U_permuted = interp1(T_model, permute(U_model, [4, 1, 2, 3]), current_time);
+    U = permute(U_permuted, [2, 3, 4, 1]);
+    V_permuted = interp1(T_model, permute(V_model, [4, 1, 2, 3]), current_time);
+    V = permute(V_permuted, [2, 3, 4, 1]);
+
+    for i = 1:length(longs)
+        if drifters(i).plot_mode
             plot( drifters(i).LONG(t), drifters(i).LAT(t), 'or' )
             hold on
         end
-    
-        
-    
-        for i = 1:length(longs_sim)
-            plot( drifters(i).LONG, drifters(i).LAT, 'k', 'MarkerSize', 12, 'LineWidth', 2)
+    end
+
+    delete(quiverplot)
+    quiverplot = quiver( LONG_data, LAT_data, U(:, :, depth_val), V(:, :, depth_val), "Color", "b");
+
+    for i = 1:length(longs)
+        if drifters(i).plot_mode
+            plot( drifters(i).LONG(drifters(i).plot_start_index:end), drifters(i).LAT(drifters(i).plot_start_index:end), 'k', 'MarkerSize', 12, 'LineWidth', 2)
             hold on
         end
-        
-        
-        %dt = T(t+1) - T(t);
+    end
     
-        for i = 1:length(longs_sim)
-            u = interp3( y_grid, x_grid, z_grid, U_mesh , drifters(i).LAT(t), drifters(i).LONG(t),t_range(t) );
-            v = interp3( y_grid, x_grid, z_grid, V_mesh , drifters(i).LAT(t), drifters(i).LONG(t),t_range(t) );
-            
-            drifters(i).LONG(t+1) = drifters(i).LONG(t) + dt * u * long_conv;
-            drifters(i).LAT(t+1) = drifters(i).LAT(t) + dt * v * lat_conv;
-        end 
-        delete(quiverplot)
-        % slicing through the 3d grid to get velocity layer at certain time
-        
-        u = interp3( y_grid, x_grid, z_grid, U_mesh , LAT_data(:,1), LONG_data(1,:),t_range(t) );
-        v = interp3( y_grid, x_grid, z_grid, V_mesh , LAT_data(:,1), LONG_data(1,:),t_range(t) );
-        quiverplot = quiver( LONG_data, LAT_data, u,v, "Color", "b");
-        
+	dt = T(t+1) - T(t);
+
+    for i = 1:length(longs)
+        if drifters(i).coasted || drifters(i).recovered
+            drifters(i).LONG(t+1) = drifters(i).LONG(t);
+            drifters(i).LAT(t+1) = drifters(i).LAT(t);
+            continue
+        end
+
+        u = interp2( LAT_data, LONG_data, U(:, :, depth_val), drifters(i).LAT(t), drifters(i).LONG(t) );
+        v = interp2( LAT_data, LONG_data, V(:, :, depth_val), drifters(i).LAT(t), drifters(i).LONG(t) );
+
+        drifters(i).LONG(t+1) = drifters(i).LONG(t) + (dt * 60 * 60)* u / abs(R * cos(drifters(i).LAT(t)));
+        drifters(i).LAT(t+1) = drifters(i).LAT(t) + (dt * 60 * 60)* v / abs(R * cos(drifters(i).LAT(t)));
+    end 
     
-        pause(1)
     
-        for i = 1:length(longs_sim)
+
+    pause(0.3)
+
+    for i = 1:length(longs)
+        if drifters(i).coasted || drifters(i).recovered 
+            continue
+        end
         
-            long_diff = drifters(i).LONG(t+1) - drifters(i).LONG(t); 
-            lat_diff = drifters(i).LAT(t+1) - drifters(i).LAT(t);
-    
-            if ((long_diff == 0) || (lat_diff == 0)) && drifters(i).coast_check
-                disp("The drifter " + num2str(i) + " has reached the coast")
-                drifters(i).coast_check = false;
-                break
-            end
+        long_diff = drifters(i).LONG(t+1) - drifters(i).LONG(t); 
+        lat_diff = drifters(i).LAT(t+1) - drifters(i).LAT(t);
+
+        if ((long_diff == 0) && (lat_diff == 0)) && ~drifters(i).coasted
+            disp("The drifter " + num2str(i) + " has reached the coast and has been recovered")
+            drifters(i).coasted = true;
+            drifters(i).recovered = true;
+            break
         end
     end
-    
-    
-    for i = 1:length(longs_sim)
-        plot( deploy_longs(:,i), deploy_lats(:,i), 'g', 'MarkerSize', 12, 'LineWidth', 2)
+
+    if (length(longs) - sum([drifters.recovered]) == 0)
+        disp("All drifters recovered.")
+        break
     end
+
+end
 
 end
 
--- MATLAB/Current/run.m
+++ MATLAB/Current/run.m
@@ -2,17 +2,14 @@ clear; clc; close all
 
 current_data = load('Data/Tbay_current_Wind_data.mat');
 
-%{
-depth = "Surface"; % Values: 1m Depth, 60cm Depth, Surface.
+longs = [6.1; 5.960; 6.068; 5.9;];
+lats = [43.05; 43.059; 43.071; 43.05];
 
-long = 6.00;
-lat = 43.05;
+% Give Time in Format: 'yyyy-MM-dd HH:mm:ss.SSS from 12-Oct-2021 00:00:00 to 14-Oct-2021 23:00:00'
 
-[LONG, LAT] = plot_trajectory(long, lat, depth, current_data);
-%}
+deploy_times = ["2021-10-12 10:00:00", "2021-10-12 11:30:00", "2021-10-13 11:00:00", "2021-10-13 15:00:00"];
+recov_times = ["2021-10-13 18:00:00", "2021-10-13 20:00:00", "2021-10-14 00:00:00", "2021-10-14 00:03:00"];
 
-longs = [6.0; 5.960; 6.068; 5.9;];
-lats = [43.05; 43.059; 43.071; 43.058];
-depth = "Surface";
+depth = 1.0;
 
-drifters = plot_multi_drifters(longs, lats, depth, current_data);
+drifters = plot_multi_drifters(longs, lats, deploy_times, recov_times, depth, current_data);
